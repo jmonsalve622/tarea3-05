a2de7259277f000f0c7dd08c5141158561afa4b7
Joaquín Monsalve <joaquinmonsalve0206@gmail.com>
2025-05-27 14:16:10 -0400

    Moneda 500 y 1000

---
+++ src/main/java/org/example/Logic/Moneda1000.java
@@ -0,0 +1,10 @@
+package org.example.Logic;
+
+public class Moneda1000 extends Moneda {
+    public Moneda1000() {
+        super();
+    }
+    public int getValor() {
+        return 1000;
+    }
+}
---
+++ src/main/java/org/example/Logic/Moneda500.java
@@ -0,0 +1,10 @@
+package org.example.Logic;
+
+public class Moneda500 extends Moneda {
+    public Moneda500() {
+        super();
+    }
+    public int getValor() {
+        return 500;
+    }
+}
