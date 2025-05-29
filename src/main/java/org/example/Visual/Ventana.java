9a16e962c100ce612878bf4af4009c294f8d137c
Joaquín Monsalve <joaquinmonsalve0206@gmail.com>
2025-05-28 23:38:57 -0400

    Prueba de ventana

--- src/main/java/org/example/Visual/Ventana.java
+++ src/main/java/org/example/Visual/Ventana.java
@@ -1,9 +1,20 @@
 package org.example.Visual;
 import javax.swing.*;
+import java.awt.BorderLayout;
 
-public class Ventana {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("FrameDemo");
+public class Ventana extends JFrame {
+    public Ventana() {
+        super();
+        this.setTitle("esta Ventana");
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(400, 300);
+        this.setVisible(true);
     }
+}
 
+class MainVentana {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+    }
 }
