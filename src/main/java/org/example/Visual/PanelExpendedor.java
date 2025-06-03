27dbbc60cc50e857b88d0e55a375ffd622394a1f
Joaquín Monsalve <joaquinmonsalve0206@gmail.com>
2025-06-03 14:07:10 -0400

    Imagenes de productos

--- src/main/java/org/example/Visual/PanelExpendedor.java
+++ src/main/java/org/example/Visual/PanelExpendedor.java
@@ -3,5 +3,10 @@ package org.example.Visual;
 import javax.swing.*;
 
 public class PanelExpendedor extends JPanel {
+     private PanelDeposito cocaDep;
+     private PanelDeposito spriteDep;
+     private PanelDeposito fantaDep;
+     private PanelDeposito super8Dep;
+     private PanelDeposito snikersDep;
 
 }
